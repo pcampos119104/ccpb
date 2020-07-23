b792cbe0e7b9bba91e1ef243fe8acece666f0b60
pcampos119104 <pcampos1191041@gmail.com>
2020-07-23 07:19:00 -0300

    search done

--- members/filters.py
+++ members/filters.py
@@ -1,11 +1,10 @@
 import  django_filters 
+from django import forms
 from .models import Member
 
 
 class MemberFilter(django_filters.FilterSet):
-    # name_member = django_filters.CharFilter(lookup_expr='icontains')
-    class Meta:
-        model = Member
-        fields = {
-                'name_member': ['icontains'],
-                }
+    name_member = django_filters.CharFilter(
+            widget=forms.TextInput(attrs={'class': 'form-control bg-light border-0 small'}),
+            lookup_expr='icontains')
+
